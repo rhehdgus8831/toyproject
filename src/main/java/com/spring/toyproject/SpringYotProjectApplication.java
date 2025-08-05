3e816ef003048676bc4a5b2424d1bf8ba89e9830
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-05 09:50:50 +0900

    파일 경로 기본 설정

--- src/main/java/com/spring/toyproject/SpringYotProjectApplication.java
+++ src/main/java/com/spring/toyproject/SpringYotProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringYotProjectApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringYotProjectApplication.class, args);
     }
