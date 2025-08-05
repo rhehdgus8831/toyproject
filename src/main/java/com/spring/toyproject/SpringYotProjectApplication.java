1bd282fecd0c6b1a46b0ca5e02348172efd13a16
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-05 09:41:59 +0900

    Initial commit

---
+++ src/main/java/com/spring/toyproject/SpringYotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringYotProjectApplication.class, args);
+    }
+
+}
