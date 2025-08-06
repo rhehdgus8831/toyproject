063be51e44c1a1e511d0bc4fbf6e3c84ec5c657f
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-06 09:48:25 +0900

    회원가입 API 응답 개선 - ApiResponse 클래스 추가 및 일관적인 응답 포맷 적용

--- src/main/java/com/spring/toyproject/api/AuthController.java
+++ src/main/java/com/spring/toyproject/api/AuthController.java
@@ -1,12 +1,12 @@
 package com.spring.toyproject.api;
 
+import com.spring.toyproject.domain.dto.common.ApiResponse;
 import com.spring.toyproject.domain.dto.request.SignUpRequest;
 import com.spring.toyproject.domain.dto.response.UserResponse;
 import com.spring.toyproject.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,16 +23,18 @@ public class AuthController {
 
     /**
      * 회원가입 API
-     * POST /api/auth/signup
+     * POST : /api/auth/signup
      */
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignUpRequest requestDto){
-        log.info("회원가입 요청 : {}",requestDto.getUsername());
+    public ResponseEntity<?> signup(@Valid @RequestBody SignUpRequest requestDto) {
+        log.info("회원가입 요청: {}", requestDto.getUsername());
 
         UserResponse response = userService.signup(requestDto);
 
-        return ResponseEntity.ok().body(response);
-
-    };
-
+        return ResponseEntity
+                .ok()
+                .body(
+                        ApiResponse.success("회원가입이 성공적으로 완료되었습니다.", response)
+                );
+    }
 }
