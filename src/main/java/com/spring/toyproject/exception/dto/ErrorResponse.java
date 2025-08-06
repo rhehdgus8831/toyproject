ef49a2f170ecca5ed05460646f8bf1b17d23acc2
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-06 10:56:16 +0900

    유효성 검증 예외 처리 추가 - ErrorResponse에 ValidationError 클래스 추가, GlobalExceptionHandler에서 MethodArgumentNotValidException 처리 로직 구현 및 값이 null인 프로퍼티 응답에서 제외하도록 JsonInclude 적용

--- src/main/java/com/spring/toyproject/exception/dto/ErrorResponse.java
+++ src/main/java/com/spring/toyproject/exception/dto/ErrorResponse.java
@@ -1,16 +1,19 @@
 package com.spring.toyproject.exception.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // 값이 null인 프로퍼티는 응답에서 제외함
 public class ErrorResponse {
 
     // 에러 발생 시간
@@ -29,6 +32,22 @@ public class ErrorResponse {
     private String path;
 
     // 유효성 검증 에러 목록
+    private List<ValidationError> validationErrors;
 
 
+    // 입력값 검증 오류 1개를 포장할 내부 클래스
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ValidationError {
+        // 에러가 난 필드명
+        private String field;
+
+        // 에러 원인 메시지
+        private String message;
+
+        // 거부된 값 (클라이언트가 뭐라고 보냈는지)
+        private Object rejectedValue;
+    }
 }
