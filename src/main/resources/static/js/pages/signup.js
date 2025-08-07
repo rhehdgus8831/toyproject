08e0a676554168e4ffd7b3a8749b4ac1d3a3f5c4
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-07 11:23:30 +0900

    페이지별 JS 모듈 로드 기능 추가 - App.js에 dynamic import 로직 구현, 페이지 모듈 초기화(init) 처리 및 오류 핸들링 로직 추가, signup.js와 login.js에서 init 메서드 반환하도록 수정

--- src/main/resources/static/js/pages/signup.js
+++ src/main/resources/static/js/pages/signup.js
@@ -1,12 +1,32 @@
-
 // 회원가입 관련 함수들의 모음
 const SignupPage = () => {
 
-    // 초기화 함수
-    const init = () => {
-        console.log(`회원가입  JS가 로딩되었습니다.`);
+    // 상태 관리 객체
+    const state = {
+        $form: null,
+        $signUpBtn: null,
+        $usernameInput: null,
+        $emailInput: null,
+        $passwordInput: null,
+        $confirmPasswordInput: null,
     };
 
+    // 초기화 함수
+    const init = () => {
+        console.log('회원가입 JS가 로딩되었습니다.');
+
+        // 필요한 DOM들을 가져오기
+        state.$form = document.getElementById('signupForm');
+        state.$signupBtn = document.getElementById('signupBtn');
+        state.$usernameInput = document.getElementById('usernameInput');
+        state.$emailInput = document.getElementById('email');
+        state.$passwordInput = document.getElementById('password');
+        state.$confirmPasswordInput = document.getElementById('confirmPassword');
+    };
+
+    return {
+        init
+    };
 };
 
 export default SignupPage;
